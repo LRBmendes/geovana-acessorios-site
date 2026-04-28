824063d512dabff5e67668558c3ef948898e44c1
LRBmendes <luis.rodrigo93@gmail.com>
2026-04-27 21:15:24 -0400

    Create layout.js

---
+++ app/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Geovana Acessórios",
+  description: "Catálogo oficial",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
